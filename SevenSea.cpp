a9d259bce3128772605dbfbd04d5e75f010f97d1
PhKhang <trannguyenphuckhang12@gmail.com>
2022-12-14 13:59:39 +0700

    First commit from another account on Private repo

--- SevenSea.cpp
+++ SevenSea.cpp
@@ -1,176 +1,203 @@
 #include <iostream>
 #include <unistd.h> // for sleep()
-#include <cstdlib> // for rand() srand()
-#include <time.h> // for time()
+#include <cstdlib>	// for rand() srand()
+#include <time.h>	// for time()
 using namespace std;
 
 #define MAX 100
 
-int size = 11;
+int mapSize = 11;
 char waterIcon = '~', shipIcon = 'X', enemyIcon = 'A', deathIcon = '#', obstacleIcon = '!', bulletIcon = '.', portalIcon = '@'; // icon game
-int shipPosX = 6, shipPosY = 9; // vi tri ban dau cua tau
-int shipStatus = 1; // 1, 9 = / -> 1
-				 	// 2, 8 = _  -> 2
-				 	// 3, 7 = \  -> 3
-					// 4, 6 = |  -> 4
-				
-char map[MAX][MAX];
-bool visited[MAX][MAX]; int visitedNum;
+int shipPosX = 6, shipPosY = 9;																									// vi tri ban dau cua tau
+int shipStatus = 1;																												// 1, 9 = / -> 1
+																																// 2, 8 = _  -> 2
+																																// 3, 7 = \  -> 3
+																																// 4, 6 = |  -> 4
 
-void initMap(char ch){
-	for (int i = 0; i < size; i++)
-		for (int j = 0; j < size; j++)
+char map[MAX][MAX];
+bool visited[MAX][MAX];
+int visitedNum;
+
+void initMap(char ch)
+{
+	for (int i = 0; i < mapSize; i++)
+		for (int j = 0; j < mapSize; j++)
 			map[i][j] = ch;
 }
 
-void updateMap(){
-	for (int i = 0; i < size; i++){
-		for (int j = 0; j < size; j++)
+void updateMap()
+{
+	for (int i = 0; i < mapSize; i++)
+	{
+		for (int j = 0; j < mapSize; j++)
 			cout << map[i][j] << " ";
-		cout << endl << endl;
+		cout << endl
+			 << endl;
 	}
 }
 
-int CheckWinCondition(){
+int CheckWinCondition()
+{
 	if (map[shipPosX][shipPosY] == deathIcon)
-		return -1; 
-		
-	for (int i = 0; i < size; i++)
-		for (int j = 0; j < size; j++)
+		return -1;
+
+	for (int i = 0; i < mapSize; i++)
+		for (int j = 0; j < mapSize; j++)
 			if (map[i][j] == enemyIcon)
 				return 0;
-	
+
 	return 1;
 }
 
-void clear(){
-	   system("cls");
+void clear()
+{
+	system("cls");
 }
 
 // Check xem co di chuyen ra ngoai map khong
-bool isInMap(int x, int y){
-	if (x >= 0 && x < size && y >= 0 && y < size)
+bool isInMap(int x, int y)
+{
+	if (x >= 0 && x < mapSize && y >= 0 && y < mapSize)
 		return true;
 	return false;
 }
 
-//Nhap gia tri cua nguoi choi
-void getInput(int &x, int &y){
-		int input;
-		cout << "Your joystick :" << endl;
-		cout << "7 8 9" << endl; // upper left  | up          | upper right
-		cout << "4 5 6" << endl; // left        | fire cannon | right
-		cout << "1 2 3" << endl; // bottom left | bottom      | bottom right 
-		cout << "Your move : ";
-		cin >> input; 
-		
-		switch(input)
-		{
-			case 7:{
-				x = -1;
-				y = -1;
-				shipStatus = 3;
-				break;
-			}
-			case 8:{
-				x = -1;
-				y = 0;	
-				shipStatus = 2;
-				break;
-			}
-			case 9:{
-				x = -1;
-				y = 1;
-				shipStatus = 1;
-				break;
-			}
-			case 6:{
-				x = 0;
-				y = 1;
-				shipStatus = 4;
-				break;
-			}		
-			case 4:{
-				x = 0;
-				y = -1;
-				shipStatus = 4;
-				break;
-			}
-			case 1:{
-				x = 1;
-				y = -1;
-				shipStatus = 1;
-				break;
-			}
-			case 2:{
-				x = 1;
-				y = 0;
-				shipStatus = 2;
-				break;
-			}
-			case 3:{
-				x = 1;
-				y = 1;
-				shipStatus = 3;
-				break;
-			}
-			default:{
-				x = 100;
-				break;
-			}
-		}
+// Nhap gia tri cua nguoi choi
+void getInput(int &x, int &y)
+{
+	int input;
+	cout << "Your joystick :" << endl;
+	cout << "7 8 9" << endl; // upper left  | up          | upper right
+	cout << "4 5 6" << endl; // left        | fire cannon | right
+	cout << "1 2 3" << endl; // bottom left | bottom      | bottom right
+	cout << "Your move : ";
+	cin >> input;
+
+	switch (input)
+	{
+	case 7:
+	{
+		x = -1;
+		y = -1;
+		shipStatus = 3;
+		break;
+	}
+	case 8:
+	{
+		x = -1;
+		y = 0;
+		shipStatus = 2;
+		break;
+	}
+	case 9:
+	{
+		x = -1;
+		y = 1;
+		shipStatus = 1;
+		break;
+	}
+	case 6:
+	{
+		x = 0;
+		y = 1;
+		shipStatus = 4;
+		break;
+	}
+	case 4:
+	{
+		x = 0;
+		y = -1;
+		shipStatus = 4;
+		break;
+	}
+	case 1:
+	{
+		x = 1;
+		y = -1;
+		shipStatus = 1;
+		break;
+	}
+	case 2:
+	{
+		x = 1;
+		y = 0;
+		shipStatus = 2;
+		break;
+	}
+	case 3:
+	{
+		x = 1;
+		y = 1;
+		shipStatus = 3;
+		break;
+	}
+	default:
+	{
+		x = 100;
+		break;
+	}
+	}
 }
 
-//Di chuyen tu vi tri (PosX, PosY) sang vi tri (PosX + x, PosY + y) 
-void enemyMove(int posX, int posY, int x, int y){
+// Di chuyen tu vi tri (PosX, PosY) sang vi tri (PosX + x, PosY + y)
+void enemyMove(int posX, int posY, int x, int y)
+{
 	// check xem vi tri di chuyen toi co vat can gi khong
-	if (map[posX + x][posY + y] != waterIcon){
-		if (map[posX + x][posY + y] == enemyIcon){
-			if (visited[posX + x][posY + y] == true){
+	if (map[posX + x][posY + y] != waterIcon)
+	{
+		if (map[posX + x][posY + y] == enemyIcon)
+		{
+			if (visited[posX + x][posY + y] == true)
+			{
 				map[posX][posY] = waterIcon;
 				map[posX + x][posY + y] = deathIcon;
 			}
-			else {
+			else
+			{
 				visited[posX][posY] = false;
 				visitedNum--;
-			}	
+			}
 		}
-		else {
+		else
+		{
 			map[posX][posY] = waterIcon;
-			map[posX + x][posY + y] = deathIcon;	
-		}		
+			map[posX + x][posY + y] = deathIcon;
+		}
 	}
-	else{
-		map[posX][posY] = waterIcon;		
+	else
+	{
+		map[posX][posY] = waterIcon;
 		map[posX + x][posY + y] = enemyIcon;
-		if (visited[posX+x][posY+y] != true){
-			visited[posX+x][posY+y] = true;	
+		if (visited[posX + x][posY + y] != true)
+		{
+			visited[posX + x][posY + y] = true;
 			visitedNum++;
-		}	
-		
+		}
 	}
-		
 }
 
-void bulletMove(int x, int y){
+void bulletMove(int x, int y)
+{
 	int temp;
 	int bullet1X = shipPosX, bullet2X = shipPosX,
-	    bullet1Y = shipPosY, bullet2Y = shipPosY;
-	for (int i = 0; i < 3; i++){
+		bullet1Y = shipPosY, bullet2Y = shipPosY;
+	for (int i = 0; i < 3; i++)
+	{
 		if (map[bullet1X][bullet1Y] != shipIcon && map[bullet1X][bullet1Y] != deathIcon)
 			map[bullet1X][bullet1Y] = waterIcon;
-		if (map[bullet1X + x][bullet1Y + y] != waterIcon){	
+		if (map[bullet1X + x][bullet1Y + y] != waterIcon)
+		{
 			map[bullet1X + x][bullet1Y + y] = deathIcon;
 		}
 		else
 			map[bullet1X + x][bullet1Y + y] = bulletIcon;
 		bullet1X += x;
 		bullet1Y += y;
-		
-		
+
 		if (map[bullet2X][bullet2Y] != shipIcon && map[bullet2X][bullet2Y] != deathIcon)
-			map[bullet2X][bullet2Y] = waterIcon;	
-		if (map[bullet2X - x][bullet2Y - y] != waterIcon){	
+			map[bullet2X][bullet2Y] = waterIcon;
+		if (map[bullet2X - x][bullet2Y - y] != waterIcon)
+		{
 			map[bullet2X - x][bullet2Y - y] = deathIcon;
 		}
 		else
@@ -183,176 +210,174 @@ void bulletMove(int x, int y){
 	}
 	// clear the last bullet
 	if (map[bullet1X][bullet1Y] != shipIcon && map[bullet1X][bullet1Y] != deathIcon)
-			map[bullet1X][bullet1Y] = waterIcon;
+		map[bullet1X][bullet1Y] = waterIcon;
 	if (map[bullet2X][bullet2Y] != shipIcon && map[bullet2X][bullet2Y] != deathIcon)
-			map[bullet2X][bullet2Y] = waterIcon;	
+		map[bullet2X][bullet2Y] = waterIcon;
 }
 
-void fireCannon(){
+void fireCannon()
+{
 	int x, y;
-	switch(shipStatus){
-		case 1:{
-			x = -1;
-			y = -1;
-			break;
-		}
-		case 2:{
-			x = 0;
-			y = 1;
-			break;
-		}
-		case 3:{
-			x = 1;
-			y = -1;
-			break;
-		}
-		case 4:{
-			x = -1;
-			y = 0;
-			break;
-		}
+	switch (shipStatus)
+	{
+	case 1:
+	{
+		x = -1;
+		y = -1;
+		break;
+	}
+	case 2:
+	{
+		x = 0;
+		y = 1;
+		break;
+	}
+	case 3:
+	{
+		x = 1;
+		y = -1;
+		break;
+	}
+	case 4:
+	{
+		x = -1;
+		y = 0;
+		break;
+	}
 	}
 	bulletMove(x, y);
 }
 
-void playerTurn(){
+void playerTurn()
+{
 	int x, y;
 	getInput(x, y);
 	if (x == 100)
 		fireCannon();
-	else
-		if (isInMap(shipPosX + x, shipPosY + y)){
-			map[shipPosX][shipPosY] = waterIcon;
-			if (map[shipPosX + x][shipPosY + y] != portalIcon){
-				if (map[shipPosX + x][shipPosY + y] != waterIcon)
-					map[shipPosX + x][shipPosY + y] = deathIcon;
-				else
-					map[shipPosX + x][shipPosY + y] = shipIcon;			
-				shipPosX += x;
-				shipPosY += y;
+	else if (isInMap(shipPosX + x, shipPosY + y))
+	{
+		map[shipPosX][shipPosY] = waterIcon;
+		if (map[shipPosX + x][shipPosY + y] != portalIcon)
+		{
+			if (map[shipPosX + x][shipPosY + y] != waterIcon)
+				map[shipPosX + x][shipPosY + y] = deathIcon;
+			else
+				map[shipPosX + x][shipPosY + y] = shipIcon;
+			shipPosX += x;
+			shipPosY += y;
+		}
+		else
+		{ // tele
+			srand(time(0));
+			int tempx, tempy;
+			while (true)
+			{
+				tempx = rand() % 11;
+				tempy = rand() % 11;
+				if (map[tempx][tempy] == waterIcon)
+					break;
 			}
-			else { // tele
-				srand(time(0));
-				int tempx, tempy;
-				while (true){
-					tempx = rand() % 11;
-					tempy = rand() % 11;
-					if (map[tempx][tempy] == waterIcon)
-						break;
-				}
-				shipPosX = tempx;
-				shipPosY = tempy;
-				map[shipPosX][shipPosY] = shipIcon;
-			}
-		}			
+			shipPosX = tempx;
+			shipPosY = tempy;
+			map[shipPosX][shipPosY] = shipIcon;
+		}
+	}
 }
 
-void enemyTurn(){
+void enemyTurn()
+{
 	int x, y;
 	visitedNum = 0;
-	for (int i = 0; i < size; i++)
-			for (int j = 0; j < size; j++)
-				visited[i][j] = false;
-				
-	while (visitedNum < size*size){
-		for (int i = 0; i < size; i++)
-			for (int j = 0; j < size; j++)
+	for (int i = 0; i < mapSize; i++)
+		for (int j = 0; j < mapSize; j++)
+			visited[i][j] = false;
+
+	while (visitedNum < mapSize * mapSize)
+	{
+		for (int i = 0; i < mapSize; i++)
+			for (int j = 0; j < mapSize; j++)
 			{
 				if (visited[i][j] == false)
 				{
 					visited[i][j] = true;
 					visitedNum++;
-					if (map[i][j] == enemyIcon){
-						
+					if (map[i][j] == enemyIcon)
+					{
+
 						// so sanh vi tri dich tai (i, j) so voi tau cua minh de co huong di toi uu nhat
 						if (i == shipPosX)
 							x = 0;
 						else if (i < shipPosX)
 							x = 1;
-						else 
+						else
 							x = -1;
-							
+
 						if (j == shipPosY)
 							y = 0;
 						else if (j < shipPosY)
 							y = 1;
-						else 
+						else
 							y = -1;
-							
-						enemyMove(i, j, x, y);	
+
+						enemyMove(i, j, x, y);
 					}
 				}
 			}
 	}
 }
 
-int main(){
-	
+int main()
+{
+
 	initMap(waterIcon);
 	// manual set up
 	map[shipPosX][shipPosY] = shipIcon;
-	
+
 	map[0][0] = portalIcon;
 	map[0][10] = portalIcon;
 	map[10][0] = portalIcon;
 	map[10][10] = portalIcon;
-	
+
 	map[6][1] = enemyIcon;
 	map[6][2] = enemyIcon;
 	map[2][5] = enemyIcon;
 	map[10][9] = enemyIcon;
 	map[1][2] = enemyIcon;
 
-	
 	map[5][9] = obstacleIcon;
 	map[1][9] = obstacleIcon;
 	map[5][4] = obstacleIcon;
-	
+
 	bool win = false;
-	
-	while (true){
+
+	while (true)
+	{
 		clear();
-		updateMap(); //  Cap nhat map sau moi lan ve xong 
-		
-		// check win/ lose condition 
-		if (CheckWinCondition() == 1){
+		updateMap(); //  Cap nhat map sau moi lan ve xong
+
+		// check win/ lose condition
+		if (CheckWinCondition() == 1)
+		{
 			win = true;
 			break;
-		} 
-		else if (CheckWinCondition() == -1){
+		}
+		else if (CheckWinCondition() == -1)
+		{
 			win = false;
-			break;	
+			break;
 		}
 		playerTurn();
-		
+
 		clear();
 		updateMap();
 		sleep(1);
-		
+
 		enemyTurn();
-		
 	}
-	
+
 	if (win)
 		cout << "YOU WIN !!!";
-	else 
-		cout << "GAME OVER !!!";  
+	else
+		cout << "GAME OVER !!!";
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
